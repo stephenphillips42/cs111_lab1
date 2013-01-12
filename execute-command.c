b613234443a0d8ea8e97e90a12d6357fec03af9e
Stephen <stephen@Stephen-Lenovo-G550.(none)>
2013-01-11 17:02:09 -0800

    Modified execute-command to make it able to compile.

--- execute-command.c
+++ execute-command.c
@@ -20,5 +20,9 @@ execute_command (command_t c, bool time_travel)
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
+  c = c;
+  time_travel = false;
+  if(time_travel) { ; }
+
   error (1, 0, "command execution not yet implemented");
 }
