1526a186add85dcfab1b98336aa2908a0b29f8eb
Stephen <stephen@Stephen-Lenovo-G550.(none)>
2013-01-25 22:09:13 -0800

    Changed the out- and input files to have .tmp at the end

--- shell-example.sh
+++ shell-example.sh
@@ -1,11 +1,11 @@
 #! /bin/bash/
 i -am # this is a comment
-nothing -c -d > bob || # this is another comment
- hi -m > stuff | #lots of comments
- y -n < things > out &&      # moar coments!! :D
- ame |is ; echo bob < instuff
-a <a1 >a2&& b && c && d || e<asdf>fdsa # comments
+nothing -c -d > bob.tmp || # this is another comment
+ hi -m > stuff.tmp | #lots of comments
+ y -n < things.tmp > out.tmp &&      # moar coments!! :D
+ ame |is ; echo bob < instuff.tmp
+a <asdf.tmp >a2.tmp&& b && c && d || e<asdf.tmp>fdsa.tmp # comments
 
 (
-hi sir < and > more
+hi sir < and.tmp > more.tmp
 (hello world;) )
