8ee721bc4aa549508cecbb34d7183bdd38ca601c
Andy Huang <andy.huang0692@gmail.com>
2013-01-26 16:15:37 -0800

    Made Execute Simple Command Function and added comments

--- execute-command.c
+++ execute-command.c
@@ -56,6 +56,39 @@ exit_with_status(int status)
 }
 
 int
+execute_simple_command (command_t c)
+{
+        int status;
+
+        // Looks for whether or not there are inputs and outputs
+        if(c->input || c->output)
+          {
+            printf ("Not implemented yet\n");
+            return -1;
+          }
+
+        // Forks the process so that the child process may run the
+        // command that was specified using execvp
+        pid_t p = fork ();
+        if (p < 0)
+          { exit(1); } // Error
+        if (p == 0)
+          {
+            // In child process
+            execvp (c->u.word[0], c->u.word);
+            exit(120); // Which exit status?
+          }
+        
+        // Wait for child process to exit
+        while (waitpid (p, &status, 0) < 0)
+          continue;
+
+        // Returns status of command execution
+        c->status = status;
+        return status;
+}
+
+int
 execute_command_helper (command_t c)
 {
   printf("entering...\n");
@@ -63,6 +96,10 @@ execute_command_helper (command_t c)
   switch (c->type)
     {
       case AND_COMMAND:
+        // Recursively runs through command tree to find simple
+        // executable commands. If the first command is successful,
+        // run the second command. If there is an error, return an error
+        // status. If both commands run successfully, return 0.
         status = execute_command_helper (c->u.command[0]);
         if (status)
             exit_with_status (status);
@@ -77,6 +114,10 @@ execute_command_helper (command_t c)
         return execute_command_helper (c->u.command[1]);
         break;
       case OR_COMMAND:
+        // Recursively runs through command tree to find simple
+        // executable commands. If the first command is not successful,
+        // run the second command. If both are not succesful, return error.
+        // Otherwise, return 0. 
         status = execute_command_helper (c->u.command[0]);
         if (status)
           return execute_command_helper (c->u.command[1]);
@@ -87,25 +128,7 @@ execute_command_helper (command_t c)
         printf ("Not implemented yet\n");
         break;
       case SIMPLE_COMMAND:
-        if(c->input || c->output)
-          {
-            printf ("Not implemented yet\n");
-            return -1;
-          }
-        pid_t p = fork ();
-        if (p < 0)
-          { exit(1); } // Error
-        if (p == 0)
-          {
-            // In child process
-            execvp (c->u.word[0], c->u.word);
-            exit(120); // Which exit status?
-          }
-        // Wait for child process to exit
-        while (waitpid (p, &status, 0) < 0)
-          continue;
-        c->status = status;
-        return status;
+        status = execute_simple_command(c);
         break;
       case SUBSHELL_COMMAND:
         return execute_command_helper (c->u.subshell_command);
