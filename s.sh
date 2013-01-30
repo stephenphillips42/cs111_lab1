5ea283bfdadbe8a13ae9fc0f533e06317ca434be
Stephen <stephen@Stephen-Lenovo-G550.(none)>
2013-01-30 09:19:26 -0800

    Added more tests to s.sh and test-exec-ok.sh, and added markers to test-exec.

--- s.sh
+++ s.sh
@@ -1,10 +1,12 @@
-echo hi > out < s.sh
+#echo hi > out < s.sh
 
-(echo hello ; ls --bob ; ) && ls
+#(echo hello ; ls --bob ; ) && ls
 
-(echo hello | tr e a) | (cat && echo failure)
+(echo hello | tr e a) | (cat - && echo failure)
 
-( cat previous_examples && cat llish.h ) | grep pid | tr aeiou ----- | cut -b 1-10 
+#(echo hello | (cat))
+
+#( cat previous_examples && cat llish.h ) | grep pid | tr aeiou ----- | cut -b 1-10 
 
 # need to deal with this ONE case ... uhg
- exec echo hello
+# exec echo hello
--- test-exec-ok.sh
+++ test-exec-ok.sh
@@ -8,45 +8,41 @@ cd "$tmp" || exit
 
 cat > test.sh <<'EOF'
 echo hello
-
+echo 1
 echo hello world i have ice cream | grep h
-
+echo 2
 (echo hello world || echo fail)
-
+echo 3
 (echo hello world && echo fail)
-
+echo 4
 echo hello world || echo --badbadbad
-
+echo 5
 echo hello world && echo --badbadbad || echo nothing to it
-
-#(echo hello world && echo fail) > test.tmp
-
+echo 6
+(echo hello world && echo fail) > test.tmp
+echo 7
 #(echo hello world && echo fail) > test.tmp | cat ./test.tmp
-
 (echo hello world | tr eo oe) | (cat - | cut -b 1-4 && echo finished)
-
+echo 8
 echo hello my world is falling apart > test2.tmp
-
+echo 9
 cat < test2.tmp | tr aeiou -----
-
+echo 10
 (echo hello ; ls --bob ; ) && ls
-
+echo 11
 (echo hello | tr e a) | (cat && echo failure)
-
-( cat previous_examples && cat llish.h ) | grep pid | tr aeiou ----- | cut -b 1-10 
+echo 12
+#( cat previous_examples && cat llish.h ) | grep pid | tr aeiou ----- | cut -b 1-10 
 
 EOF
-bash test.sh > test.exp
+bash test.sh > test.exp 2>test.experr
 
 ../timetrash test.sh >test.out 2>test.err || exit
 
 diff -u test.exp test.out || exit
-test ! -s test.err || {
-  cat test.err
-  exit 1
-}
+diff -u test.experr test.err || exit
 
-) || exit
+) || echo LAST ALMOST WHY && exit
 
 rm -fr "$tmp"
 
